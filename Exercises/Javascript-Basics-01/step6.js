357dba0dca0e9bcb60b0ce330776c9aa05e92844
Michel Al Achkar <michelalachkar@gmail.com>
2019-12-16 12:09:21 +0200

    js basics step 6 completed

---
+++ Exercises/Javascript-Basics-01/step6.js
@@ -0,0 +1,8 @@
+let firstNum = prompt("please provide us with the first number");
+let secondNum = prompt("please provide us with the second number");
+
+let divide = () => {
+  return firstNum / secondNum;
+};
+
+alert("the answer of your division is :" + divide());
