3aefb60f29d1fe29f7b6b27a756585e84fa734f4
Michel Al Achkar <michelalachkar@gmail.com>
2020-01-13 13:41:46 +0200

    Weather API:HTML&CSS

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -10,10 +10,89 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header">
-          <h1>Michel Al Achkar</h1>
+        <header className="app_header">
+          <div className="search_bar">
+            <input
+              type="text"
+              name="city"
+              className="search_field"
+              placeholder="location"
+            />
+            <input
+              className="search_button"
+              type="button"
+              name="submit"
+              value="FIND WEATHER"
+            />
+          </div>
         </header>
-        <main className="app__main"></main>
+        <main className="app_main">
+          <div className="current_weather">
+            <div className="imgAndInfo">
+              <img src="http://placekitten.com/200/300" />
+              <p>overcast clouds</p>
+            </div>
+            <div className="info">
+              <div className="temperature">
+                <p>
+                  <strong>Temperature</strong>
+                </p>
+                <p>10&deg; to 11&deg; C</p>
+              </div>
+              <div className="otherWeatherInfo">
+                <div className="humidity">
+                  <p>
+                    <strong>Humidity</strong>
+                  </p>
+                  <p>78%</p>
+                </div>
+                <div className="pressure">
+                  <p>
+                    <strong>Pressure</strong>
+                  </p>
+                  <p>1008.48</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="hourlyWeather">
+            <div>
+              <p>03:00</p>
+              <img src="http://placekitten.com/200/300" />
+              <p>8&deg;C</p>
+            </div>
+            <div>
+              <p>06:00</p>
+              <img src="http://placekitten.com/200/300" />
+              <p>9&deg;C</p>
+            </div>
+            <div>
+              <p>09:00</p>
+              <img src="http://placekitten.com/200/300" />
+              <p>14&deg;C</p>
+            </div>
+            <div>
+              <p>12:00</p>
+              <img src="http://placekitten.com/200/300" />
+              <p>17&deg;C</p>
+            </div>
+            <div>
+              <p>15:00</p>
+              <img src="http://placekitten.com/200/300" />
+              <p>18&deg;C</p>
+            </div>
+            <div>
+              <p>18:00</p>
+              <img src="http://placekitten.com/200/300" />
+              <p>16&deg;C</p>
+            </div>
+            <div>
+              <p>21:00</p>
+              <img src="http://placekitten.com/200/300" />
+              <p>13&deg;C</p>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
