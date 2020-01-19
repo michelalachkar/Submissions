5d34bed48751d1fdeb1409b9101e8570ccfaf112
Michel Al Achkar <michelalachkar@gmail.com>
2020-01-19 23:47:10 +0200

    Count & Filter

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -1,12 +1,26 @@
 "select * from students"
+
 "SELECT name from students"
+
 "SELECT * FROM students WHERE Age>30"
+
 "SELECT name FROM students WHERE Gender='F' AND Age>30"
+
 "SELECT Points FROM students WHERE name  = 'Alex'"
+
 "INSERT INTO students (Name, Age,Points,Gender) VALUES ('Michel',26,1000,'M')"
+
 "UPDATE students SET Points = Points +20 WHERE name = 'Basma'"
+
 "UPDATE students SET Points = Points -10 WHERE name = 'Alex'"
-"CREATE TABLE graduates (ID INT AUTO_INCREMENT NOT NULL  PRIMARY KEY, Name TEXT NOT NULL UNIQUE, Age INT, Gender TEXT ,Points INT, Graduation TEXT)"
+
+"CREATE TABLE graduates (
+    ID INT AUTO_INCREMENT NOT NULL  PRIMARY KEY, 
+    Name TEXT NOT NULL UNIQUE, 
+    Age INT, 
+    Gender TEXT ,
+    Points INT, 
+    Graduation TEXT)"
 
 "INSERT INTO graduates (ID, Name, Age, Gender, Points)
    SELECT ID, Name, Age, Gender, Points 
@@ -29,7 +43,18 @@
     INNER JOIN companies ON employees.Company=companies.name
     WHERE companies.date < 2000 "
 
-"SELECT companies.name
+"SELECT companies.Name
     FROM companies
     INNER JOIN employees ON companies.Name=employees.Company
     WHERE employees.Role = 'Graphic Designer'"
+
+"SELECT Name FROM (SELECT Name ,MAX(Points) FROM students)"
+
+"SELECT AVG(Points) as AveragePoints FROM students"
+
+"SELECT COUNT(Name) FROM students WHERE Points = 500"
+
+"SELECT name FROM students WHERE name LIKE '%s%'"
+
+"SELECT name FROM students ORDER BY Points DESC"
+
