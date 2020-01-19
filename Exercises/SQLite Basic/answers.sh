839ddd27362d6397448648ac42c2c7f51ef5f9de
Michel Al Achkar <michelalachkar@gmail.com>
2020-01-19 23:14:40 +0200

    Basic Queries

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,8 @@
+"select * from students"
+"SELECT name from students"
+"SELECT * FROM students WHERE Age>30"
+"SELECT name FROM students WHERE Gender='F' AND Age>30"
+"SELECT Points FROM students WHERE name  = 'Alex'"
+"INSERT INTO students (Name, Age,Points,Gender) VALUES ('Michel',26,1000,'M')"
+"UPDATE students SET Points = Points +20 WHERE name = 'Basma'"
+"UPDATE students SET Points = Points -10 WHERE name = 'Alex'"
