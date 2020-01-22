2d22a085921a91fffa749b145f1c342cc1bf9aee
michelalachkar <michelalachkar@gmail.com>
2020-01-22 12:29:22 +0200

    step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -7,7 +7,13 @@ const port = 5000;
 app.get("/",(req,res)=>{
     res.send('now');
 });
-
+app.get("/test",(req,res)=>{
+    res.send({"status":200,"message":"ok"});
+});
+app.get("/time",(req,res)=>{
+    let myDate = new Date();
+res.send({"status":200,"message":`${myDate.getHours()}:${myDate.getMinutes()}`});
+});
 app.listen(port,()=>{
     console.log(`Server listenning on http://localhost:${port}`);
 })
