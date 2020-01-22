7fe4b956e74a20b81cb1062cb2c22bd36390ea92
michelalachkar <michelalachkar@gmail.com>
2020-01-22 13:00:37 +0200

    step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -14,6 +14,23 @@ app.get("/time",(req,res)=>{
     let myDate = new Date();
 res.send({"status":200,"message":`${myDate.getHours()}:${myDate.getMinutes()}`});
 });
+
+app.get('/hello/:id',(req,res)=>{
+    const myId = req.params.id;
+    res.send({"status":200,"message":`hello ${myId}`});
+});
+
+app.get('/search?',(req,res)=>{
+    if(req.query.s){
+        const mySearch = req.query.s;
+   
+        res.send({"status":200,"message":"ok","data":mySearch});
+    }else{
+        res.send({"status":500,"error":true,"message":"you have to provide a search"})
+    }
+    
+});
+
 app.listen(port,()=>{
     console.log(`Server listenning on http://localhost:${port}`);
 })
