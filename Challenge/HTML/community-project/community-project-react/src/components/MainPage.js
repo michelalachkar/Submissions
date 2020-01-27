890fed0e45cd8e09898b8d22861165440b9c6912
michelalachkar <michelalachkar@gmail.com>
2020-01-27 15:41:43 +0200

    work in progress

---
+++ Challenge/HTML/community-project/community-project-react/src/components/MainPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function MainPage() {
+    return (
+        <div className="container">
+            <section className="mainSection intro" id="intro">
+                <h2>Weclome</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae assumenda ducimus nobis eligendi iste nulla fuga, quaerat hic soluta optio, sed deleniti impedit quasi, non suscipit voluptas id veniam vel natus quos ipsam corporis praesentium! Commodi veritatis repellat architecto ducimus.</p>
+            </section>
+            <section className="mainSection news" id="news">
+                <h2>News</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ea in sequi optio assumenda cumque.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ea in sequi optio assumenda cumque.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ea in sequi optio assumenda cumque.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ea in sequi optio assumenda cumque.</p>
+
+            </section>
+            <section className="mainSection contactUs" id="contact">
+                <h2>Contact Us</h2>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, quo error fugiat temporibus, expedita illo explicabo debitis modi voluptates inventore facilis nam. Aspernatur reiciendis suscipit voluptas quaerat cumque distinctio alias. Cum ea voluptatum cumque sunt quisquam vitae suscipit nobis voluptas sequi expedita dolor fugiat, iste rerum blanditiis? Adipisci, reiciendis sed!</p>
+            </section>
+        </div>
+    )
+}
---
+++ Challenge/HTML/community-project/community-project-react/src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+        <div className="navbar">
+        <div className="titleLinks">
+            <div className="title">
+                <h1>The Sky is The Limit</h1>
+                <p>Where we bring your ideas to life</p>
+            </div>
+            <div className="navLinks">
+                <a href="#intro" >Home</a>
+                <a href="#news" >News</a>
+                <a href="#contact">Contact Us</a>
+            </div>
+        </div>
+        </div>
+    )
+}
